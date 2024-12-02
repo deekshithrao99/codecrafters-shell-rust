cf0e03f75f4fa38e8319b496ce5757db7bffeada
deekshithrao99 <deekshithrao07@gmail.com>
2024-12-02 15:34:23 +0530

    added exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -15,6 +16,13 @@ fn main() {
     io::stdout().flush().unwrap();
     stdin.read_line(&mut input).unwrap();
     input.pop();
+    //check if input is empty or unreconisged
+    if input.is_empty() {
+        continue;
+    } else if input == "exit 0" {
+        break;
+        
+    }
     
     println!("{}: command not found",input.trim());
         
